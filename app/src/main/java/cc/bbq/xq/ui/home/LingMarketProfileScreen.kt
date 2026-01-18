05964ee3eb8aab7e4941f8bc24d998b782f9b2d9
Voltual1 <18206039173@163.com>
2026-01-18 15:34:38 +0800

    更新

--- app/src/main/java/cc/bbq/xq/ui/home/LingMarketProfileScreen.kt
+++ app/src/main/java/cc/bbq/xq/ui/home/LingMarketProfileScreen.kt
@@ -22,9 +22,11 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import cc.bbq.xq.AppStore
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.clickable
 import androidx.navigation.NavController
 import cc.bbq.xq.LingMarketClient
 import cc.bbq.xq.ui.AccountProfile
