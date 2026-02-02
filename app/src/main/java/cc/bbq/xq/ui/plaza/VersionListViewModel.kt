27a94672b24df11346baeaef1ff94b14e3f5c652
Voltual1 <18206039173@163.com>
2026-02-02 10:58:26 +0800

    更新

--- app/src/main/java/cc/bbq/xq/ui/plaza/VersionListViewModel.kt
+++ app/src/main/java/cc/bbq/xq/ui/plaza/VersionListViewModel.kt
@@ -1,3 +1,23 @@
+//Copyright (C) 2025 Voltual
+// 本程序是自由软件：你可以根据自由软件基金会发布的 GNU 通用公共许可证第3版
+//（或任意更新的版本）的条款重新分发和/或修改它。
+//本程序是基于希望它有用而分发的，但没有任何担保；甚至没有适销性或特定用途适用性的隐含担保。
+// 有关更多细节，请参阅 GNU 通用公共许可证。
+//
+// 你应该已经收到了一份 GNU 通用公共许可证的副本
+// 如果没有，请查阅 <http://www.gnu.org/licenses/>.
+package cc.bbq.xq.ui.plaza
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import cc.bbq.xq.AppStore
+import cc.bbq.xq.data.repository.IAppStoreRepository
+import cc.bbq.xq.data.unified.UnifiedAppItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
 class VersionListViewModel(
     application: Application,
     private val repositories: Map<AppStore, IAppStoreRepository>
